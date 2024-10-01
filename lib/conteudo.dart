9e0c2a237c079f4b41e9838fd01c8dda3eee5644
alcs5 <alcs5@aluno.ifal.edu.br>
2024-10-01 01:05:39 +0000

    primeira

---
+++ lib/conteudo.dart
@@ -0,0 +1,122 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class conteudo extends StatefulWidget {
+ const conteudo({super.key});
+
+
+
+
+ @override
+ State<conteudo> createState() => _conteudoState();
+}
+
+
+
+
+class _conteudoState extends State<conteudo> {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     backgroundColor: Color(0xFFE8E8E8),
+     body: Column(
+       children: [
+         Container(
+           height: 200,
+           width: 400,
+           child: Row(
+             mainAxisAlignment: MainAxisAlignment.start,
+             children: [
+               SizedBox(
+                 width: 10,
+               ),
+               Expanded(
+                 child: Text(
+                   "Conteúdos que mais caem no enem!",
+                   style: GoogleFonts.montserrat(
+                     fontWeight: FontWeight.bold,
+                     fontSize: 26,
+                   ),
+                 ),
+               ),
+             ],
+           ),
+           decoration: BoxDecoration(
+             color: Color(0xFF66A9CF),
+           ),
+         ),
+         SizedBox(
+           height: 8,
+
+
+
+
+         ),
+         Padding(padding: const EdgeInsets.all(16),
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.center,
+             children: [
+
+
+
+
+               ElevatedButton(
+                 onPressed: () {},
+                 child: Text(
+                   "Linguagens",
+                   style: GoogleFonts.montserrat(
+                       color: Colors.black,
+                       fontSize: 15,
+                       fontWeight: FontWeight.bold),
+                 ),
+                 style: ElevatedButton.styleFrom(
+                   minimumSize: Size(294, 87),
+                   backgroundColor: Color(0xFFC8D9DF),
+                 ),
+               ),
+
+
+
+
+               ElevatedButton(
+                 onPressed: () {},
+                 child: Text(
+                   "Linguagens",
+                   style: GoogleFonts.montserrat(
+                       color: Colors.black,
+                       fontSize: 15,
+                       fontWeight: FontWeight.bold),
+                 ),
+                 style: ElevatedButton.styleFrom(
+                   minimumSize: Size(294, 87),
+                   backgroundColor: Color(0xFFBFD6D1),
+
+
+                 ),
+               ),
+
+
+
+
+
+
+
+
+
+
+
+
+             ],
+           ),
+         ),
+       ],
+     ),
+   );
+ }
+}
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:myapp/conteudo.dart';
+
+
+void main() {
+ runApp(
+   const MaterialApp(
+     home: conteudo(),
+   ),
+ );
+}
