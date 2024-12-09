363b6c4a287eeacb22e0b567dae44c9d7210c87e
Alessandra <ass112@aluno.ifal.edu.br>
2024-12-09 17:36:13 -0300

    "Atualizando 17:36"

---
+++ lib/db/dbA.dart
@@ -0,0 +1,18 @@
+import 'package:myapp/domain/telahistoria.dart';
+
+class Database {
+  static List conteudos = [
+    Telinha(
+      titulo: 'HISTÓRIA GERAL',
+      iconPath: 'assets/barco.png',
+      link:
+      "https://www.youtube.com/watch?v=ym5aER-r2tI&list=PLMra4G0-Z7pNwIVhWAzLuVZJA_1FgM69t",
+    ),
+    Telinha(
+      titulo: 'HISTÓRIA DO BRASIL',
+      iconPath: 'assets/punho.png',
+      link:
+      "https://www.youtube.com/watch?v=LSqOKMnakU4&list=PLMra4G0-Z7pPPztK7F69zHzpzAtaHEnJ5",
+    ),
+  ];
+}
---
+++ lib/domain/telahistoria.dart
@@ -0,0 +1,11 @@
+class Telinha {
+  String titulo;
+  String iconPath;
+  String link;
+
+  Telinha({
+    required this.titulo,
+    required this.iconPath,
+    required this.link,
+  });
+}
